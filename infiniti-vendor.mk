ae44d63b13cf2956a8e0f3448b181579006fcd06
chandu078 <chandudyavanapelli03@gmail.com>
2026-06-12 19:16:41 +0000

    infiniti: Update from OOS 11.A.42
    Change-Id: I041e56cbc3cab47fe5631ace8bb6de044c24158a

--- infiniti-vendor.mk
+++ infiniti-vendor.mk
@@ -979,7 +979,9 @@ PRODUCT_COPY_FILES += \
     vendor/oneplus/infiniti/proprietary/odm/firmware/tp/infiniti/libafepanel_s3910_boe.so:$(TARGET_COPY_OUT_ODM)/firmware/tp/infiniti/libafepanel_s3910_boe.so \
     vendor/oneplus/infiniti/proprietary/odm/firmware/tp/infiniti/libafepanel_s3910_tianma.so:$(TARGET_COPY_OUT_ODM)/firmware/tp/infiniti/libafepanel_s3910_tianma.so \
     vendor/oneplus/infiniti/proprietary/odm/firmware/tp/infiniti/libpostalgo.so:$(TARGET_COPY_OUT_ODM)/firmware/tp/infiniti/libpostalgo.so \
+    vendor/oneplus/infiniti/proprietary/odm/firmware/tp/infiniti/libpostalgo_symbol.so:$(TARGET_COPY_OUT_ODM)/firmware/tp/infiniti/libpostalgo_symbol.so \
     vendor/oneplus/infiniti/proprietary/odm/firmware/tp/infiniti/libprealgo.so:$(TARGET_COPY_OUT_ODM)/firmware/tp/infiniti/libprealgo.so \
+    vendor/oneplus/infiniti/proprietary/odm/firmware/tp/infiniti/libprealgo_symbol.so:$(TARGET_COPY_OUT_ODM)/firmware/tp/infiniti/libprealgo_symbol.so \
     vendor/oneplus/infiniti/proprietary/odm/firmware/tp/infiniti/main_synaptics-s3910_boe_aging_limit.csv:$(TARGET_COPY_OUT_ODM)/firmware/tp/infiniti/main_synaptics-s3910_boe_aging_limit.csv \
     vendor/oneplus/infiniti/proprietary/odm/firmware/tp/infiniti/main_synaptics-s3910_boe_limit.csv:$(TARGET_COPY_OUT_ODM)/firmware/tp/infiniti/main_synaptics-s3910_boe_limit.csv \
     vendor/oneplus/infiniti/proprietary/odm/firmware/tp/infiniti/main_synaptics-s3910_tianma_aging_limit.csv:$(TARGET_COPY_OUT_ODM)/firmware/tp/infiniti/main_synaptics-s3910_tianma_aging_limit.csv \
